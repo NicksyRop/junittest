c37233871d4c467241356c3a68e12070489d8ffd
NicksyRop <nicksonkipkorir25@gmail.com>
2023-07-02 10:06:20 +0300

    added test case one

---
+++ src/test/java/com/example/testing/TestingApplicationTests.java
@@ -0,0 +1,39 @@
+package com.example.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class TestingApplicationTests {
+
+	@Test
+	void itShouldAddTwoNumbers() {
+
+		Calculator calculator = new Calculator();
+		//given
+
+		int numOne = 20;
+		int numTwo = 50;
+		//when
+
+		int result = calculator.add(numOne,numTwo);
+
+
+		//then
+
+		assertThat(result).isEqualTo(70);
+	}
+
+	class Calculator{
+
+		int add( int a , int b){
+
+			return  a+b;
+		}
+
+
+	}
+
+}
